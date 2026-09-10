65462fab0fff4be1ff0d722e61c2667fad53ce64
rohsigma <30771000+rohsigma@users.noreply.github.com>
2026-09-10 16:53:33 +0200

    tidying up

--- admin/class-rohsigma-any-api-connector-admin.php
+++ admin/class-rohsigma-any-api-connector-admin.php
@@ -69,18 +69,6 @@ class Rohsigma_Any_Api_Connector_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Rohsigma_Any_Api_Connector_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Rohsigma_Any_Api_Connector_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/rohsigma-any-api-connector-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -91,18 +79,6 @@ class Rohsigma_Any_Api_Connector_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Rohsigma_Any_Api_Connector_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Rohsigma_Any_Api_Connector_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/rohsigma-any-api-connector-admin.js?vdate=' . gmdate( 'YmdHis' ), array( 'jquery' ), $this->version, false );
 
