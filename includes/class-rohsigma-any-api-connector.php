b9f37aef5ea8e299ff5207daeb3a39018d42c8c3
rohsigma <30771000+rohsigma@users.noreply.github.com>
2026-09-13 17:41:42 +0200

    Remove REST routes registration from admin hooks
    Removed the action for registering REST routes from the admin hooks.

--- includes/class-rohsigma-any-api-connector.php
+++ includes/class-rohsigma-any-api-connector.php
@@ -157,7 +157,6 @@ class Rohsigma_Any_Api_Connector {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
-		$this->loader->add_action( 'rest_api_init', $plugin_admin, 'register_rest_routes' );
 	}
 
 	/**
