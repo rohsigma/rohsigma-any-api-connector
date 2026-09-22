22bf9bda82e49d486e3e1491fdb628cf0e70ee4e
rohsigma <30771000+rohsigma@users.noreply.github.com>
2026-09-22 10:39:45 +0200

    Change author email and link in documentation
    Updated author email and link in the plugin documentation.

--- admin/class-rohsigma-any-api-connector-admin.php
+++ admin/class-rohsigma-any-api-connector-admin.php
@@ -3,7 +3,7 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * @link       https://rohsigma.com
+ * @link       https://rohsigma.es
  * @since      1.0.0
  *
  * @package    Rohsigma_Any_Api_Connector
@@ -18,7 +18,7 @@
  *
  * @package    Rohsigma_Any_Api_Connector
  * @subpackage Rohsigma_Any_Api_Connector/admin
- * @author     rohsigma <rich@rohsigma.com>
+ * @author     rohsigma <rich@rohsigma.es>
  */
 class Rohsigma_Any_Api_Connector_Admin {
 
